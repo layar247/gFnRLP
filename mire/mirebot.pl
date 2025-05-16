0d8c50bccdf7ebcfe16d232e31a2395a78a2e866
Владислав <vladcepurko@gmail.com>
2025-05-17 00:19:27 +0300

    read

--- mire/mirebot.pl
+++ mire/mirebot.pl
@@ -1,26 +1,23 @@
 :- use_module(library(socket)).
 
 :- dynamic exit/1.
-:- dynamic name_sent/0.  % Флаг, что имя отправлено
 
-% Грамматика для разбора exits
 e(north) --> [north].
 e(south) --> [south].
-e(west) --> [west].
-e(east) --> [east].
-exits([Exit]) --> e(Exit).
+e(west)  --> [west].
+e(east)  --> [east].
+
+exits([Exit])     --> e(Exit).
 exits([Exit|Exits]) --> e(Exit), exits(Exits).
+
 parse_exits(Exits) --> [exits], exits(Exits), ['.'].
 
-% Разбор токенов
-parse(Tokens) :- 
-    phrase(parse_exits(Exits), Tokens, Rest), 
-    retractall(exit(_)), 
+parse(Tokens) :-
+    phrase(parse_exits(Exits), Tokens, _),
+    retractall(exit(_)),
     assert(exit(Exits)).
-
 parse(_).
 
-% Фильтрация спецсимволов
 filter_codes([], []).
 filter_codes([H|T1], T2) :-
     char_code(C, H),
@@ -30,39 +27,38 @@ filter_codes([H|T1], [F|T2]) :-
     code_type(F, to_lower(H)),
     filter_codes(T1, T2).
 
-% Отправка имени при первом подключении
-send_name(Stream) :-
-    \+ name_sent,
-    format(atom(Name), "bot~n", []),
-    write(Stream, Name),
-    flush_output(Stream),
-    assertz(name_sent).
-
-% Отправка движения, если есть exits
-send_move(Stream) :-
+process(Stream) :-
     exit([Direction|_]),
-    format(atom(Command), "move ~w~n", [Direction]),
+    format(atom(Command), 'move ~w~n', [Direction]),
+    write(Command),
     write(Stream, Command),
     flush_output(Stream),
     retractall(exit(_)).
+process(_).
 
-% Главный цикл
 loop(Stream) :-
     read_line_to_codes(Stream, Codes),
-    (Codes == end_of_file -> 
-        close(Stream)
-    ; 
-        filter_codes(Codes, Filtered),
-        atom_codes(Atom, Filtered),
-        tokenize_atom(Atom, Tokens),
-        parse(Tokens),
-        send_move(Stream),
-        loop(Stream)
-    ).
+    filter_codes(Codes, Filtered),
+    atom_codes(Atom, Filtered),
+    tokenize_atom(Atom, Tokens),
+    write(Tokens),
+    parse(Tokens),
+    nl,
+    flush(),
+    sleep(1),
+    process(Stream),
+    loop(Stream).
 
-% Точка входа
+send_name(Stream) :-
+    format(Stream, "prolog_bot~n", []),
+    flush_output(Stream).
+
 main :-
     setup_call_cleanup(
         tcp_connect(localhost:3333, Stream, []),
-        (send_name(Stream), loop(Stream)),
-        close(Stream)).
+        (
+            send_name(Stream),  % <-- Отправка имени игрока
+            loop(Stream)
+        ),
+        close(Stream)
+    ).
