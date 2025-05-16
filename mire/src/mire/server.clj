2f3ab708210cee99cd211a781cd805235d30a8c5
Владислав <vladcepurko@gmail.com>
2025-05-17 01:17:10 +0300

    read

--- mire/src/mire/server.clj
+++ mire/src/mire/server.clj
@@ -42,7 +42,7 @@
                 player/*money* 0
                 player/*current-chest* nil]
         (println "Инициализация игрока...") ;; сюда
-
+	(println "Комната: " @player/*current-room*)
         (dosync
          (commute (:inhabitants @player/*current-room*) conj player/*name*)
          (commute player/streams assoc player/*name* *out*))
