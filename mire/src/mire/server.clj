f892fe572e4feef0b9b1a234228fddcbfc48f17d
Владислав <vladcepurko@gmail.com>
2025-05-17 00:57:31 +0300

    read

--- mire/src/mire/server.clj
+++ mire/src/mire/server.clj
@@ -2,83 +2,55 @@
   (:gen-class)
   (:require [clojure.java.io :as io]
             [server.socket :as socket]
+            [clojure.core.async :as async]
             [mire.player :as player]
             [mire.commands :as commands]
-            [mire.rooms :as rooms]
-            [mire.letters :as letters]
-            [mire.items :as items]
-            [mire.chests :as chests]
-            [clojure.string :as str])
-(defn- cleanup []
-  (dosync
-   (doseq [item @player/*inventory*]
-     (commands/discard item))
-   (commute player/streams dissoc player/*name*)
-   (commute (:inhabitants @player/*current-room*)
-            disj player/*name*)))
+            [mire.rooms :as rooms]))
 
-(defn- get-unique-player-name [name]
-  (if (@player/streams name)
-    (do (print "Name taken, try again: ") (flush) (recur (read-line)))
-    name))
+(defn- log [& args]
+  (binding [*out* *err*]
+    (apply println args)))
+
+(defn- cleanup [name room]
+  (when (and name room)
+    (dosync
+     (commute (:inhabitants @room) disj name)
+    (commute player/streams dissoc name)))
+
+(defn- handle-commands [in out name]
+  (binding [*in* (io/reader in)
+            *out* (io/writer out)]
+    (loop []
+      (when-let [input (.readLine *in*))]
+        (try
+          (let [response (commands/execute input)]
+            (.write *out* (str response "\n"))
+            (.flush *out*))
+          (catch Exception e
+            (log "Command error:" e)))
+        (recur))))
 
 (defn- mire-handle-client [in out]
-  (try
-    (binding [*in* (io/reader in)
-              *out* (io/writer out)
-              *err* (io/writer System/err)]
-      
-      (println "\nWelcome to Mire!")  ; Добавлено приветствие
-      (print "What is your name? ") (flush)
-      
-      (binding [player/*name* (get-unique-player-name (read-line))
-                player/*current-room* (ref (or (@rooms/rooms :start)
-                                              (do (println "FATAL: No start room!") (System/exit 1))))
-                player/*inventory* (ref #{})
-                player/*luck* 20
-                player/*money* 0
-                player/*current-chest* nil]
-        
-        (dosync
-         (commute (:inhabitants @player/*current-room*) conj player/*name*)
-         (commute player/streams assoc player/*name* *out*))
-        
-        (println "\n" (commands/look))  ; Добавлен перенос строки
-        (print (player/health-bar)) 
-        (println " Money:" player/*money*)
-        (print player/prompt) (flush)
-        
-        (loop [input (read-line)]
-          (when input
-            (try
-              (println (commands/execute input))
-              (catch Exception e 
-                (println "Error:" (.getMessage e))))
-            (print (player/health-bar)) (flush)
-            (println " Money:" player/*money*)
-            (print player/prompt) (flush)
-            (recur (read-line)))))
-    
-    (catch Exception e
-      (println "Connection error:" (.getMessage e)))
-    (finally 
-      (cleanup))))
+  (future
+    (try
+      (let [name (loop []
+                   (.write *out* "What is your name? ")
+                   (.flush *out*)
+                   (let [name (.readLine *in*))]
+                     (if (empty? name)
+                       (recur)
+                       name))]
+        (binding [player/*name* name
+                  player/*current-room* (ref (@rooms/rooms :start))]
+          (handle-commands in out name))
+      (finally
+        (cleanup player/*name* @player/*current-room*)))))
 
-(defn -main
-  ([port dir]
-   (println "Loading game data from" dir)
-   (rooms/add-rooms (str dir "rooms"))
-   (chests/add-chests (str dir "chests"))
-   (letters/add-letters (str dir "letters"))
-   (items/add-items (str dir "items"))
-   
-   (when (empty? @rooms/rooms)
-     (println "ERROR: No rooms loaded! Check your rooms files.")
-     (System/exit 1))
-   
-   (println "Starting server on port" port)
-   (defonce server (socket/create-server (Integer. port) mire-handle-client))
-   (println "Server ready!"))
+(defn -main [& args]
+  (rooms/add-rooms "resources/rooms")
+  (when-not (@rooms/rooms :start)
+    (log "ERROR: No :start room defined")
+    (System/exit 1))
   
-  ([port] (-main port "resources/"))
-  ([] (-main 3333)))
+  (socket/create-server 3333 mire-handle-client)
+  (log "Server running on port 3333"))
